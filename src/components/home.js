baf22eff2ce87beaaf5398560e3cb8ac0630d3ea
Rony Chammai <ronychammai123@gmail.com>
2025-01-01 20:31:26 +0200

    certification title changed

--- src/components/home.js
+++ src/components/home.js
@@ -8,7 +8,7 @@ function Home() {
         <h1 className="name">Karen Abdel Khalek</h1>
         <p className="profession">Interior Architect & Project Developer</p>
         <p className="tagline">Transforming ideas into visually stunning experiences</p>
-        <a href="#projects" className="cta-button">Download My CV</a>
+        <a className="cta-button">Download My CV</a>
       </div>
     </section>
   );
